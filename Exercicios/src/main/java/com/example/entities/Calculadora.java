de420e84bb7f0b6c79a40907e855b3b14f0bf7c7
Luis Martins <125165369+martinssluis@users.noreply.github.com>
2026-05-26 10:27:20 -0300

    feat: reolucao do exercicio

---
+++ Exercicios/src/main/java/com/example/entities/Calculadora.java
@@ -0,0 +1,10 @@
+package com.example.entities;
+
+public class Calculadora {
+
+    double numero;
+
+    public double retornaDobro(double numero){
+        return numero*2;
+    }
+}
---
+++ Exercicios/src/main/java/com/example/entities/Pessoa.java
@@ -0,0 +1,8 @@
+package com.example.entities;
+
+public class Pessoa {
+
+    public void dizOlaMundo(){
+        System.out.println("Olá mundo!");
+    }
+}
