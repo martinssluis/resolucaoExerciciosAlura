07099f1c94acfe20e79a0292b52ba043155fee83
Luis Martins <luishenrique.martinslima@gmail.com>
2026-05-31 21:25:23 -0300

    feat(/entities/ContaBancaria): cria super classe ContaBancaria

--- src/main/java/com/example/entites/ContaBancaria.java
+++ src/main/java/com/example/entites/ContaBancaria.java
@@ -1,4 +1,21 @@
 package com.example.entites;
 
 public class ContaBancaria {
+    protected double saldo;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double depositar(double valor){
+        return saldo += valor;
+    }
+
+    public double sacar(double valor){
+        return saldo -= valor;
+    }
+
+    public double verSaldo(){
+        return saldo;
+    }
 }
