d094a0cb609266c7fea26c6f49011a6e42dd2f75
Luis Martins <luishenrique.martinslima@gmail.com>
2026-06-01 19:07:06 -0300

    fix(/entites/ContaCOrrente): apaga atributo desncecessario tarifa mensal e tira o this do return, fazendo com que o metodo retorne o saldo - tarifaMensal digitada pelo usuario

--- src/main/java/com/example/entites/ContaCorrente.java
+++ src/main/java/com/example/entites/ContaCorrente.java
@@ -1,9 +1,10 @@
 package com.example.entites;
 
 public class ContaCorrente extends ContaBancaria{
-    private double tarifaMensal;
-    
+
+    // dados que podem ser calculados a partir de outros não devem virar atributos (tarifaMensal)
     public double cobrarTarifaMensal(double tarifaMensal){
-        return saldo -= this.tarifaMensal;
+        return saldo -= tarifaMensal;
+        // com o this(referenciava atributo exluido) pegavamos o valor do atributo e não valor digitado pelo usuario
     }
 }
