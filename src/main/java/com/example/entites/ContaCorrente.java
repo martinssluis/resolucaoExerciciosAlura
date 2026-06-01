2cff1b88917ee673217a659627db04faa967f75e
Luis Martins <luishenrique.martinslima@gmail.com>
2026-05-31 21:26:54 -0300

    feat(/entities/ContaBancaria): cria sube classe que herda de ContaBancaria e tem metodo cobrarTarifaMensal

--- src/main/java/com/example/entites/ContaCorrente.java
+++ src/main/java/com/example/entites/ContaCorrente.java
@@ -1,4 +1,9 @@
 package com.example.entites;
 
-public class ContaCorrente {
+public class ContaCorrente extends ContaBancaria{
+    private double tarifaMensal;
+    
+    public double cobrarTarifaMensal(double tarifaMensal){
+        return saldo -= this.tarifaMensal;
+    }
 }
