0c90d976e895b4ba392160b65efa64eb3e5101b9
Luis Martins <luishenrique.martinslima@gmail.com>
2026-05-31 21:22:04 -0300

    feat(/entites/Cachorro): cria classe Cachorro que herda de Animal, com sobrescrita de emitirSom e tem metodo abanarRabo

--- src/main/java/com/example/entites/Cachorro.java
+++ src/main/java/com/example/entites/Cachorro.java
@@ -1,4 +1,13 @@
 package com.example.entites;
 
-public class Cachorro {
+public class Cachorro extends Animal{
+
+    @Override
+    public void emitirSom(){
+        System.out.println("Au Au");
+    }
+
+    public void abanarRabo(){
+        System.out.printf("%s abanou rabo\n", nome);
+    }
 }
