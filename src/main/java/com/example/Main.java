48d718496f7674c73b020b9afa2239aca2e5396c
Luis Martins <luishenrique.martinslima@gmail.com>
2026-06-01 19:07:36 -0300

    feat(Main): cria instancias de ContaBancaria e ContaCorrente

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,11 +1,11 @@
 package com.example;
 
 
-import com.example.entites.Animal;
-import com.example.entites.Cachorro;
-import com.example.entites.Gato;
+import com.example.entites.*;
 
 import java.util.Scanner;
+
+import static com.example.services.Validacoes.lerDouble;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,24 +13,44 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
        // ANIMAIS ******************************************************************************************************
-        Animal loro = new Animal();
-        System.out.print("Qual o nome do animal? ");
-        loro.setNome(sc.nextLine());
-        loro.emitirSom();
+//        Animal loro = new Animal();
+//        System.out.print("Qual o nome do animal? ");
+//        loro.setNome(sc.nextLine());
+//        loro.emitirSom();
+//
+//        Cachorro cachorro = new Cachorro();
+//        System.out.print("Qual o nome do cachorro? ");
+//        cachorro.setNome(sc.nextLine());
+//        cachorro.emitirSom();
+//        cachorro.abanarRabo();
+//
+//        Gato gato = new Gato();
+//        System.out.print("Qual o nome do gato? ");
+//        gato.setNome(sc.nextLine());
+//        gato.emitirSom();
+//        gato.arranharMOveis();
 
-        Cachorro cachorro = new Cachorro();
-        System.out.print("Qual o nome do cachorro? ");
-        cachorro.setNome(sc.nextLine());
-        cachorro.emitirSom();
-        cachorro.abanarRabo();
+        // Conta *******************************************************************************************************
+        ContaBancaria minhaConta = new ContaBancaria();
+        System.out.print("Quanto você quer depositar na sua Conta bancária? ");
+        minhaConta.depositar(lerDouble(sc));
+        System.out.println("Saldo de R$" + minhaConta.consultarSaldo());
+        System.out.println("Quanto você quer sacar da sa Conta Bancária?");
+        minhaConta.sacar(lerDouble(sc));
+        System.out.println("Saldo de R$" + minhaConta.consultarSaldo());
 
-        Gato gato = new Gato();
-        System.out.print("Qual o nome do gato? ");
-        gato.setNome(sc.nextLine());
-        gato.emitirSom();
-        gato.arranharMOveis();
-        //**************************************************************************************************************
+        ContaCorrente contaCorrente = new ContaCorrente();
+        System.out.print("Quanto você quer depositar na sua Conta Corrente?");
+        contaCorrente.depositar(lerDouble(sc));
+        System.out.println("Saldo de R$" + contaCorrente.consultarSaldo());
+        System.out.print("Quanto você quer sacar da sa Conta Corrente?");
+        contaCorrente.sacar(lerDouble(sc));
+        System.out.println("Saldo de R$" +contaCorrente.consultarSaldo());
 
+        System.out.print("Quanto é a tarifa mensal? ");
+        contaCorrente.cobrarTarifaMensal(lerDouble(sc));
+        System.out.println("Saldo de R$" +contaCorrente.consultarSaldo());
+        
         sc.close();
     }
     }
