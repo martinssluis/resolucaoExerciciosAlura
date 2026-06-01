caac3ea52ec48ff067c23c9a706a3b2722ca8ab9
Luis Martins <luishenrique.martinslima@gmail.com>
2026-05-31 21:22:48 -0300

    feat(/entites/Gato): cria classe Gato que herda de Animal, com sobrescrita de emitirSom e tem metodo arranharMOveis

--- src/main/java/com/example/entites/Gato.java
+++ src/main/java/com/example/entites/Gato.java
@@ -1,4 +1,13 @@
 package com.example.entites;
 
-public class Gato {
+public class Gato extends Animal{
+
+    @Override
+    public void emitirSom(){
+        System.out.println("Miau Miau");
+    }
+
+    public void arranharMOveis(){
+        System.out.printf("%s arranhou os móveis\n", nome);
+    }
 }
