8eb025d31529ed270ddfa7e822af1bbda989b738
Luis <martinsluisdev@gmail.com>
2026-06-03 07:15:58 -0300

    feat(Main): cria instância de ModeloCarro

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,7 @@ package com.example;
 
 
 import com.example.entites.*;
+import com.example.services.Validacoes;
 
 import java.util.Scanner;
 
@@ -31,26 +32,39 @@ public class Main {
 //        gato.arranharMOveis();
 
         // Conta *******************************************************************************************************
-        ContaBancaria minhaConta = new ContaBancaria();
-        System.out.print("Quanto você quer depositar na sua Conta bancária? ");
-        minhaConta.depositar(lerDouble(sc));
-        System.out.println("Saldo de R$" + minhaConta.consultarSaldo());
-        System.out.println("Quanto você quer sacar da sa Conta Bancária?");
-        minhaConta.sacar(lerDouble(sc));
-        System.out.println("Saldo de R$" + minhaConta.consultarSaldo());
+//        ContaBancaria minhaConta = new ContaBancaria();
+//        System.out.print("Quanto você quer depositar na sua Conta bancária? ");
+//        minhaConta.depositar(lerDouble(sc));
+//        System.out.println("Saldo de R$" + minhaConta.consultarSaldo());
+//        System.out.println("Quanto você quer sacar da sa Conta Bancária?");
+//        minhaConta.sacar(lerDouble(sc));
+//        System.out.println("Saldo de R$" + minhaConta.consultarSaldo());
+//
+//        ContaCorrente contaCorrente = new ContaCorrente();
+//        System.out.print("Quanto você quer depositar na sua Conta Corrente?");
+//        contaCorrente.depositar(lerDouble(sc));
+//        System.out.println("Saldo de R$" + contaCorrente.consultarSaldo());
+//        System.out.print("Quanto você quer sacar da sa Conta Corrente?");
+//        contaCorrente.sacar(lerDouble(sc));
+//        System.out.println("Saldo de R$" +contaCorrente.consultarSaldo());
+//
+//        System.out.print("Quanto é a tarifa mensal? ");
+//        contaCorrente.cobrarTarifaMensal(lerDouble(sc));
+//        System.out.println("Saldo de R$" +contaCorrente.consultarSaldo());
 
-        ContaCorrente contaCorrente = new ContaCorrente();
-        System.out.print("Quanto você quer depositar na sua Conta Corrente?");
-        contaCorrente.depositar(lerDouble(sc));
-        System.out.println("Saldo de R$" + contaCorrente.consultarSaldo());
-        System.out.print("Quanto você quer sacar da sa Conta Corrente?");
-        contaCorrente.sacar(lerDouble(sc));
-        System.out.println("Saldo de R$" +contaCorrente.consultarSaldo());
+        // Carro *******************************************************************************************************
+        ModeloCarro carro1 = new ModeloCarro();
+        System.out.print("Qual o modelo do carro?");
+        carro1.setNome(sc.nextLine());
+        for (int i=0;i<3;i++){
+            System.out.println("Qual o ano do carro? ");
+            Integer ano = Validacoes.lerInteiro(sc);
+            System.out.println("Qual o preço do carro nesse ano? ");
+            Double preco = Validacoes.lerDouble(sc);
+            carro1.setPrecoPorAno(ano,preco);
+            carro1.exibirInformacoes();
+        }
 
-        System.out.print("Quanto é a tarifa mensal? ");
-        contaCorrente.cobrarTarifaMensal(lerDouble(sc));
-        System.out.println("Saldo de R$" +contaCorrente.consultarSaldo());
-        
         sc.close();
     }
     }
