578fbddf0a274af187da61bda089470fe2a9d44c
Luis <martinsluisdev@gmail.com>
2026-06-03 15:01:38 -0300

    feat(NumerosPrimos): cria metodo verificarPrimalidaade

--- src/main/java/com/example/entites/NumerosPrimos.java
+++ src/main/java/com/example/entites/NumerosPrimos.java
@@ -3,5 +3,16 @@ package com.example.entites;
 public class NumerosPrimos {
 
     //TODO: verificarPrimalidade()
+    public boolean verificarPrimalidade(Integer numero){
+        for(int i=2; i<Math.sqrt(numero); i++){
+            int resultado = numero%i;
+            if (resultado ==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     //TODO: listarPrimos()
 }
