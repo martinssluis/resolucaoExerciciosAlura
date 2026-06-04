0b21892fe9fb389d4b790757b2b35fc09e23a95b
Luis Martins <luishenrique.martinslima@gmail.com>
2026-06-04 14:26:24 -0300

    feat(/entities/VerificadorPrimo): cria verificarSeEhPrimo

--- src/main/java/com/example/entites/VerificadorPrimo.java
+++ src/main/java/com/example/entites/VerificadorPrimo.java
@@ -2,5 +2,12 @@ package com.example.entites;
 
 public class VerificadorPrimo extends NumerosPrimos{
 
-    //TODO: verificarSeEhPrimo()
+    public void verificarSeEhPrimo(Integer numero){
+    boolean ehPrimo = verificarPrimalidade(numero); // ao chamar o metodo, ele espera um parametro
+        if (ehPrimo){ // se o resultado de verificarPrimalidade() for true
+            System.out.print("O número é primo");
+        }else{
+            System.out.print("O número não é primo");
+        }
+    }
 }
