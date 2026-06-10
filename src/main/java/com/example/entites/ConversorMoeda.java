9e59f0bbc0b522caab004870ff7883ca1c56eba9
Luis <martinsluisdev@gmail.com>
2026-06-10 15:03:25 -0300

    feat(/entities/ConversosMoeda): cria classe e implemeta ConversaoFinanceira

--- src/main/java/com/example/entites/ConversorMoeda.java
+++ src/main/java/com/example/entites/ConversorMoeda.java
@@ -1,4 +1,12 @@
 package com.example.entites;
 
-public class ConversorMoeda {
+import com.example.services.ConversaoFinanceira;
+
+public class ConversorMoeda implements ConversaoFinanceira {
+
+
+    @Override
+    public double converterDolarParaReal(double valor) {
+        return valor * 5;
+    }
 }
