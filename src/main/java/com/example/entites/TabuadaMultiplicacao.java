5ae2a04795bb3da87999429a2eb1a589b54b9cf0
Luis <martinsluisdev@gmail.com>
2026-06-16 15:16:51 -0300

    feat(TabuadaMultiplicacao): cria classe que implementa interface Tabuada, mas sem logica do metodo

--- src/main/java/com/example/entites/TabuadaMultiplicacao.java
+++ src/main/java/com/example/entites/TabuadaMultiplicacao.java
@@ -1,4 +1,31 @@
 package com.example.entites;
 
-public class TabuadaMultiplicacao {
+import com.example.services.Tabuada;
+
+public class TabuadaMultiplicacao implements Tabuada {
+    int numero;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    //TODO: criar forma de calcular a tabuada
+    //for i<=1= exibir numero
+    // ex: 3
+    //3x{i=1}=3 i++
+    //3x{i=2} =6
+
+    public void calculartabuada(int numero){
+        for(int i=0;i<=10;i++){
+        }
+
+    }
+    @Override
+    public void mostrarTabuada() {
+
+    }
 }
