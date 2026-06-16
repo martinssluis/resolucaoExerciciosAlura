55b32b41976e2444b34c01995f847bdc0be74c10
Luis <martinsluisdev@gmail.com>
2026-06-16 07:23:13 -0300

    feat(CalduladoraSalaRetangular): cria classe com atributos altura e largura, e implementa interface CalculoGeometrico

--- src/main/java/com/example/entites/CalculadoraSalaRetangular.java
+++ src/main/java/com/example/entites/CalculadoraSalaRetangular.java
@@ -1,4 +1,34 @@
 package com.example.entites;
 
-public class CalculadoraSalaRetangular {
+import com.example.services.CalculoGeometrico;
+
+public class CalculadoraSalaRetangular implements CalculoGeometrico {
+
+    double altura, largura;
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return altura * largura;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return (altura*2) + (largura*2);
+    }
 }
