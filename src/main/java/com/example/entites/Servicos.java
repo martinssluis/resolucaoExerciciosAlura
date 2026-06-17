294627c2b5127a4486290e6bbc0043eab9ac7bc3
Luis <martinsluisdev@gmail.com>
2026-06-17 09:48:45 -0300

    feat(PServicos: cria classe que implementa interface Vendavel, e cria logica de  dobrar desconto para mais de 10 produtos

--- src/main/java/com/example/entites/Servicos.java
+++ src/main/java/com/example/entites/Servicos.java
@@ -1,4 +1,46 @@
 package com.example.entites;
 
-public class Servicos {
+import com.example.services.Vendavel;
+
+import javax.sound.midi.Soundbank;
+
+public class Servicos implements Vendavel {
+
+    private double preco;
+    private int quantidade;
+    private double desconto;
+
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+
+    @Override
+    public double precoTotal() {
+        if(quantidade > 10){
+            System.out.println("Por ter feito mais de 10 serviços, seu desconto será dobrado!");
+            return preco - (desconto*2);
+        }
+        return  preco - desconto;
+    }
 }
