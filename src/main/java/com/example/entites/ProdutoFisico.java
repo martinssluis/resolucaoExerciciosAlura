9ee7c5e959cf8a35522d77281222a4258f8eabbf
Luis <martinsluisdev@gmail.com>
2026-06-17 09:03:16 -0300

    feat(ProdutoFisico): cria classe com os atributos preco e descontoBlackFriday, e implementa metodos da interface Calculavel

--- src/main/java/com/example/entites/ProdutoFisico.java
+++ src/main/java/com/example/entites/ProdutoFisico.java
@@ -1,4 +1,30 @@
 package com.example.entites;
 
-public class ProdutoFisico {
+import com.example.services.Calculavel;
+
+public class ProdutoFisico implements Calculavel {
+
+    private double preco;
+    private double descontoBlackFriday;
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getDescontoBlackFriday() {
+        return descontoBlackFriday;
+    }
+
+    public void setDescontoBlackFriday(double descontoBlackFriday) {
+        this.descontoBlackFriday = descontoBlackFriday;
+    }
+
+    @Override
+    public double calcularPrecoFinal() {
+        return preco - descontoBlackFriday;
+    }
 }
