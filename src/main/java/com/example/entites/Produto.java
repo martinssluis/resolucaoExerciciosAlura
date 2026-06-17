4a954546007b479af671d169ea5323096f080a40
Luis <martinsluisdev@gmail.com>
2026-06-17 09:47:38 -0300

    feat(Produto): cria classe que implementa interface Vendavel, e cria logica de limitar desconto para até 3 produtos

--- src/main/java/com/example/entites/Produto.java
+++ src/main/java/com/example/entites/Produto.java
@@ -1,4 +1,46 @@
 package com.example.entites;
 
-public class Produuto {
+import com.example.services.Vendavel;
+
+public class Produto implements Vendavel {
+
+    private double preco;
+    private int quantidade;
+    private double descontoPrimeiraCompra;
+
+    //TODO se quantidade for maior que 3, o desconto não é aplicavel
+
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getDescontoPrimeiraCompra() {
+        return descontoPrimeiraCompra;
+    }
+
+    public void setDescontoPrimeiraCompra(double descontoPrimeiraCompra) {
+        this.descontoPrimeiraCompra = descontoPrimeiraCompra;
+    }
+
+    @Override
+    public double precoTotal() {
+        if (quantidade > 3){
+            System.out.println("Por ter mais de 3 produtos, o desconto não será aplicado");
+            return preco;
+        }
+        return preco - descontoPrimeiraCompra;
+    }
 }
