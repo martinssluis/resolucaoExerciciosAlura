78fdea67ab7ecc289d6f232f111b55085d92eaf1
Luis <martinsluisdev@gmail.com>
2026-06-17 08:35:31 -0300

    feat(ConversorTemperaturaPadrao): cria classe que implementa interface ConversorTemperatura e cria logica das conversoes

--- src/main/java/com/example/entites/ConversosTemperaturaPadrao.java
+++ src/main/java/com/example/entites/ConversosTemperaturaPadrao.java
@@ -1,4 +1,27 @@
 package com.example.entites;
 
-public class ConversosTemperaturaPadrao {
+import com.example.services.ConversorTemperatura;
+
+
+public class ConversosTemperaturaPadrao implements ConversorTemperatura {
+
+    private double temperatura;
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    @Override
+    public void celsiusParaFahrenheit() {
+        System.out.println((this.temperatura * 9/5) + 32 + "ºF");
+
+    }
+    @Override
+    public void fahrenheitParaCelsius() {
+        System.out.println((this.temperatura - 32) * 5/9 + "ºC");
+    }
 }
