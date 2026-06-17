b63f999fac79f69322139f30a5fca1a9bab1f2ce
Luis <martinsluisdev@gmail.com>
2026-06-17 09:04:08 -0300

    feat(Livro): cria classe com atributos preco e taxaEntrega, e implementa metodo da interface Calculavel

--- src/main/java/com/example/entites/Livro.java
+++ src/main/java/com/example/entites/Livro.java
@@ -1,4 +1,30 @@
 package com.example.entites;
 
-public class Livro {
+import com.example.services.Calculavel;
+
+public class Livro implements Calculavel {
+
+    private double preco;
+    private double taxaEntrega;
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getTaxaEntrega() {
+        return taxaEntrega;
+    }
+
+    public void setTaxaEntrega(double taxaEntrega) {
+        this.taxaEntrega = taxaEntrega;
+    }
+
+    @Override
+    public double calcularPrecoFinal() {
+        return preco + taxaEntrega;
+    }
 }
