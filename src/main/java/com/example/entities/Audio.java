6416adbd68298943f0e7b47021a2fefb1ef32034
Luis <martinsluisdev@gmail.com>
2026-06-23 14:29:19 -0300

    feat(Audio): criaa classe base

---
+++ src/main/java/com/example/entities/Audio.java
@@ -0,0 +1,35 @@
+package com.example.entities;
+
+public class Audio {
+    private String titulo;
+    private int totalReproducoes, totalCurtidas;
+    private double classificacao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public double getClassificacao() {
+        return classificacao;
+    }
+
+    public void curte(){
+        this.totalCurtidas++;
+    }
+
+    public void reproduz(){
+        this.totalReproducoes++;
+    }
+}
