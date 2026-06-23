8a9125cb1e9693078c6e2ed7ff45bc8cb3c39ffe
Luis <martinsluisdev@gmail.com>
2026-06-23 14:31:17 -0300

    feat(Podcast): cria classe que implementa Audio

--- src/main/java/com/example/entities/Podcast.java
+++ src/main/java/com/example/entities/Podcast.java
@@ -1,4 +1,22 @@
 package com.example.entities;
 
-public class Podcast {
+public class Podcast extends Audio{
+
+    private String apresentador, descicao;
+
+    public String getApresentador() {
+        return apresentador;
+    }
+
+    public void setApresentador(String apresentador) {
+        this.apresentador = apresentador;
+    }
+
+    public String getDescicao() {
+        return descicao;
+    }
+
+    public void setDescicao(String descicao) {
+        this.descicao = descicao;
+    }
 }
