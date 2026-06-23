12b9be7e32e0992faf0ec9c3adb1fc7ef0a8be29
Luis <martinsluisdev@gmail.com>
2026-06-23 14:30:38 -0300

    feat(Musica): cria classe que implementa Audio

--- src/main/java/com/example/entities/Musica.java
+++ src/main/java/com/example/entities/Musica.java
@@ -1,4 +1,29 @@
 package com.example.entities;
 
-public class Musica {
+public class Musica extends Audio{
+    private String album, cantor, genero;
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getCantor() {
+        return cantor;
+    }
+
+    public void setCantor(String cantor) {
+        this.cantor = cantor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 }
