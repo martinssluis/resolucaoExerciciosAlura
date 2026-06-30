9106e61aa5acf3cec015a74ce08f5376acd702f2
Luis <martinsluisdev@gmail.com>
2026-06-30 10:18:18 -0300

    feat(Main): cria instancia de Pessoa

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,16 +1,24 @@
 package com.example;
 
 
-import com.example.entites.*;
-import com.example.services.Validacoes;
+import com.example.entities.Pessoa;
 
 import java.util.Scanner;
+
+import static com.example.services.Validacoes.lerInteiro;
 
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        Pessoa eu = new Pessoa();
+        System.out.print("Iforme o nome da pessoa: ");
+        eu.setNome(sc.nextLine());
+        System.out.print("Informe a idade da pessoa: ");
+        eu.setIdade(lerInteiro(sc));
+
+        System.out.println(eu.toString());
 
         sc.close();
     }
