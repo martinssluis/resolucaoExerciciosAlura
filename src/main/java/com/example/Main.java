6a627870a233654f15ade07c45bac685223f1096
Luis <martinsluisdev@gmail.com>
2026-06-30 10:31:45 -0300

    fix(Main): limpa arquivo principal

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,7 @@
 package com.example;
 
-
-import com.example.entites.*;
-import com.example.services.Validacoes;
-
-import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
     }
     }
