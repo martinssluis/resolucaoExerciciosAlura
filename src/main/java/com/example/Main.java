66f672e8dc5cea9edf5578558831383706a94173
Luis <martinsluisdev@gmail.com>
2026-07-01 10:31:47 -0300

    feat(Main): cria instancia de Produto

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,29 @@
 package com.example;
 
+import com.example.entities.Produto;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        //TODO: criar ArrayList de produtos
+        Produto mouse = new Produto();
+        mouse.setNome("Mouse");
+        mouse.setPreco(65.90);
+        mouse.setQuantidade(2);
+
+        Produto teclado = new Produto();
+        teclado.setNome("Teclado");
+        teclado.setPreco(120.00);
+        teclado.setQuantidade(1);
+
+        ArrayList<Produto> carrinhoDeCompras = new ArrayList<>();
+        carrinhoDeCompras.add(mouse);
+        carrinhoDeCompras.add(teclado);
+        //TODO: imprimir o tamanho da lista
+        System.out.println("O tamanho do meu carrinho de compras é: " + carrinhoDeCompras.size());
+        //TODO: recuperar um produto pelo íncide
+        System.out.println("Segundo item no  meu carrinho de compras: " + carrinhoDeCompras.get(1));
     }
     }
