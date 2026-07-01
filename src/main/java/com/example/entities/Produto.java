015bce90245d65b2ad2369d9a39d45e1297190b2
Luis <martinsluisdev@gmail.com>
2026-07-01 10:29:32 -0300

    feat(Produto): cria classe com atributos nome, preco e quantidade e sobrescreve metodo toString

---
+++ src/main/java/com/example/entities/Produto.java
@@ -0,0 +1,40 @@
+package com.example.entities;
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    //TODO: construtor que aceite parametros para inicializar os atributos
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    //TODO: implementar metodo toString para retornar yma representação em texto do objeto
+    @Override
+    public String toString() {
+        return "Produto: " + this.nome + " com valor de R$" + this.preco + " quantidade: " + this.quantidade;
+    }
+}
