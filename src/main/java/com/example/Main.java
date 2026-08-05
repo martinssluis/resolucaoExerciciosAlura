6044099313f0791838d427bc32af5dc7414514c5
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-05 10:14:59 -0300

    feat(Main): cria lista de números inteiros e usa o Collections.sort para ordenar a lista de forma crescente e imprimi-la

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,21 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Integer> listaInteiros = new ArrayList<Integer>();
+        listaInteiros.add(3);
+        listaInteiros.add(5);
+        listaInteiros.add(2);
+        listaInteiros.add(6);
+        listaInteiros.add(1);
+        listaInteiros.add(4);
+        System.out.println("Lista desordenada: " + listaInteiros);
+        Collections.sort(listaInteiros);
+        System.out.println("Lista ordenada: " +listaInteiros);
 
     }
     }
