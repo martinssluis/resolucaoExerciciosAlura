62d7c1df413a8ba3ca6b5f9fec00b2e65911dd48
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-11 17:58:37 -0300

    feat(Pessoa): cria entidade pessoa com atributos nome, idade e cidade com construtor padrão e construtor para PesoaDto e cria toString

--- src/main/java/com/example/entities/Pessoa.java
+++ src/main/java/com/example/entities/Pessoa.java
@@ -1,4 +1,30 @@
 package com.example.entities;
 
+import com.example.dto.PessoaDTO;
+
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private String cidade;
+
+    public Pessoa(String nome, int idade, String cidade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cidade = cidade;
+    }
+
+    public Pessoa(PessoaDTO pessoaDTO) {
+        this.nome = pessoaDTO.nome();
+        this.idade = pessoaDTO.idade();
+        this.cidade = pessoaDTO.cidade();
+    }
+
+    @Override
+    public String toString() {
+        return "nome=" + nome + '\'' +
+                ", idade=" + idade + "," +
+                "cidade= " + cidade;
+    }
+
+
 }
