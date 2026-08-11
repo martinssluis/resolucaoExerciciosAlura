e63901c92254ddaeb382491fea88c3c149f1d7a1
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-11 20:18:29 -0300

    feat(Main): cria json que contem as informações do Livro, incluindo um objeto com as informações da editora, cria as instancias de Livro e LivroDTo, converte o json de livro para um elemento para buscar apenas o objeto de Editora e criar um novo json a partir disso, criando assim as intancias de Editora e EditoraDTO

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,10 +1,12 @@
 package com.example;
 
+import com.example.dto.EditoraDTO;
+import com.example.dto.LivroDTO;
 import com.example.dto.PessoaDTO;
+import com.example.entities.Editora;
+import com.example.entities.Livro;
 import com.example.entities.Pessoa;
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,22 +26,50 @@ public class Main {
                 }
                 """;
 
+        String livroQueEstouLendo = """
+                {
+                    "Nome": "Hipotese do Amor",
+                    "Autor": "Ali Hazelwood",
+                    "Editora": {
+                        "Nome": "Arqueiro",
+                        "Fundacao": "2011"
+                    } 
+                }
+                """;
+
 
         System.out.println("Json:" + jsonPessoa);
 
         Gson gson = new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
                 .create();
+
         PessoaDTO pessoaDTO = gson.fromJson(jsonPessoa, PessoaDTO.class);
         System.out.println(pessoaDTO);
         Pessoa pessoa = new Pessoa(pessoaDTO);
         System.out.println("Pessoa convertida: ");
         System.out.println(pessoa);
 
-        PessoaDTO novaPessoaDTO = gson.fromJson(jsonPessoaSemCidade, PessoaDTO.class);
         System.out.println(jsonPessoaSemCidade);
+        PessoaDTO novaPessoaDTO = gson.fromJson(jsonPessoaSemCidade, PessoaDTO.class);
         Pessoa outraPessoa = new Pessoa(novaPessoaDTO);
         System.out.println("Outra Pessoa convertida: ");
         System.out.println(outraPessoa);
+
+
+        System.out.println(livroQueEstouLendo);
+        LivroDTO livroDTO = gson.fromJson(livroQueEstouLendo, LivroDTO.class);
+        System.out.println(livroDTO);
+        Livro meuLivro = new Livro(livroDTO);
+        System.out.println("Meu Livro: " + meuLivro);
+
+        JsonElement element = JsonParser.parseString(livroQueEstouLendo);
+        JsonObject jsonLivro = element.getAsJsonObject();
+        JsonObject jsonEditora = jsonLivro.getAsJsonObject("Editora");
+
+        EditoraDTO editoraArqueiro = gson.fromJson(jsonEditora, EditoraDTO.class);
+        System.out.println(editoraArqueiro);
+        Editora editora = new Editora(editoraArqueiro);
+        System.out.println("Editora Arqueiro: " + editora);
     }
     }
