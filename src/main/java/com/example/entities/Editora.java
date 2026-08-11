5b0048ec95eac0317903f53a0b50ae3be5484c22
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-11 20:16:06 -0300

    feat(Editora): cria entidade Editora

--- src/main/java/com/example/entities/Editora.java
+++ src/main/java/com/example/entities/Editora.java
@@ -1,4 +1,27 @@
 package com.example.entities;
 
+import com.example.dto.EditoraDTO;
+
 public class Editora {
+
+    private String nome;
+    private int anoFundacao;
+
+    public Editora(String nome, int anoFundacao) {
+        this.nome = nome;
+        this.anoFundacao = anoFundacao;
+    }
+
+    public Editora(EditoraDTO editoraDTO){
+        this.nome = editoraDTO.nome();
+        this.anoFundacao = Integer.valueOf(editoraDTO.fundacao());
+    }
+
+    @Override
+    public String toString() {
+        return "Editora{" +
+                "nome='" + nome + '\'' +
+                ", anoFundacao=" + anoFundacao +
+                '}';
+    }
 }
