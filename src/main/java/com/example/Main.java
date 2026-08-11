82752387e206133a09a634d651e6f34bff40e26a
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-11 18:00:23 -0300

    feat(Main): cria json de pessoa, inicializa um gson, cria instancia de PessoaDTO que recebe esse json ja formatado pelo gson, cria instancia de Pessoa que recebe pessoaDTO

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,32 @@
 package com.example;
 
+import com.example.dto.PessoaDTO;
+import com.example.entities.Pessoa;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Main {
     public static void main(String[] args) {
 
+
+        String json = """
+                {
+                    "Nome": "Luis",
+                    "Idade": "23",
+                    "Cidade": "Pindamonhangaba"
+                }
+                """;
+
+        System.out.println("Json:" + json);
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+        PessoaDTO pessoaDTO = gson.fromJson(json, PessoaDTO.class);
+        System.out.println(pessoaDTO);
+        Pessoa pessoa = new Pessoa(pessoaDTO);
+        System.out.println("Pessoa convertida: ");
+        System.out.println(pessoa);
     }
     }
