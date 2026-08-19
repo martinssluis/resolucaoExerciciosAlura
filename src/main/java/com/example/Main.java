91c00fdf156af733fae99c463102cc92fb96b721
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-19 15:25:00 -0300

    feat(Main): cria arquivo principal que recebe nome de usuário e passa como parametro no metodo do githubHttpClient para buscarUsuario com tratamento de exceções

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,29 @@
 package com.example;
 
+import com.example.client.GithubHttpClient;
+import com.example.exceptions.ConectionErrorException;
+import com.example.exceptions.UserNotFoundException;
+
+import java.net.http.HttpClient;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o seu usuário no github: ");
+        String usuario = sc.nextLine();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        GithubHttpClient githubHttpClient = new GithubHttpClient(httpClient);
+
+        try{
+            String respostaAPI = githubHttpClient.buscarUsuario(usuario);
+            System.out.println("===RESPOSTA DA API===");
+            System.out.println(respostaAPI);
+        }
+        catch (UserNotFoundException e){System.out.println("Ops! Parece que ocorreu um erro inesperado: " +e.getMessage());}
+        catch (ConectionErrorException e){System.out.println("Ops! Parece que ocorreu um erro inesperado: " +e.getMessage());}
     }
     }
