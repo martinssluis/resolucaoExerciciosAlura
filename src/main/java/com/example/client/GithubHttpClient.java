01c3fa76d20ff17dd43f5ec778a28ba419d9ff5e
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-19 15:23:16 -0300

    feat(GithubHttpClient): cria arquivo dedicado para conexão com API do Github com tratamento de exceções para error de conexão e usuario nao encontrado

--- src/main/java/com/example/client/GithubHttpClient.java
+++ src/main/java/com/example/client/GithubHttpClient.java
@@ -1,4 +1,39 @@
 package com.example.client;
 
+import com.example.exceptions.ConectionErrorException;
+import com.example.exceptions.UserNotFoundException;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
+
 public class GithubHttpClient {
+
+    private HttpClient client;
+    private static final String BASE_URL = "https://api.github.com/users/";
+
+    // para construir uma nova coneção com a API e guardar na memoria
+    public GithubHttpClient(HttpClient client) {this.client = client;}
+
+    public String buscarUsuario(String usuario) throws UserNotFoundException, ConectionErrorException{
+        String endpoitGetUser = BASE_URL+usuario;
+
+        try{
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(endpoitGetUser))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() ==404){ throw new UserNotFoundException("Usuário " + usuario+ " não encontrado no GitHub");}
+
+        if (response.statusCode() !=200){ throw new ConectionErrorException("Erro na requisição: Status" +  response.statusCode());}
+
+        return response.body();
+    }
+        catch (UserNotFoundException | ConectionErrorException e){throw e;}
+        catch (Exception e){ throw new ConectionErrorException("Erro ao conectar: " + e.getMessage());}
+    }
 }
