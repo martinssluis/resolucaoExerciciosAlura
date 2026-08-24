0536ccffc4b8627c1ce40ce1da9dfdfbffe087d6
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-24 20:49:10 -0300

    feat(Main): cria arquivo e escreve mensagem no arquivo criado

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        FileWriter arquivo = new FileWriter("arquivo.txt");
+        arquivo.write("Conteúdo a ser gravado no arquivo");
+        arquivo.close();
 
     }
     }
