6498725b2b9e8c2111b42003831d593a235bb34a
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-24 20:59:32 -0300

    feat(Titulo): cria classe Titulo com atributos nome e nota

--- src/main/java/com/example/entity/Titulo.java
+++ src/main/java/com/example/entity/Titulo.java
@@ -1,4 +1,11 @@
 package com.example.entity;
 
 public class Titulo {
+    private String nome;
+    private int nota;
+
+    public Titulo(String nome, int nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
 }
