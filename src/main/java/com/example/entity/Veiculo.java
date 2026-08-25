891a33d73f5a3371a3be2f4c50d5f8ffc043767b
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-24 21:04:12 -0300

    feat(Veiculo): cria classe Veiculo com atributos nome e ano

---
+++ src/main/java/com/example/entity/Veiculo.java
@@ -0,0 +1,11 @@
+package com.example.entity;
+
+public class Veiculo {
+    private String nome;
+    private int ano;
+
+    public Veiculo(String nome, int ano) {
+        this.nome = nome;
+        this.ano = ano;
+    }
+}
