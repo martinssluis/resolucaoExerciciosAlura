c72e8ce8dc7bd512711a73acb1b12b690f1892d9
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-24 21:05:33 -0300

    feat(Main): cria objeto corolla que instacia Veiculo, cria variavel corollaJson que recebe esse objeto convertido para json e imprime

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.example.entity.Titulo;
+import com.example.entity.Veiculo;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.FieldNamingPolicy;
@@ -8,15 +9,15 @@ import com.google.gson.FieldNamingPolicy;
 public class Main {
     public static void main(String[] args) {
 
-        Titulo meuTitulo = new Titulo("Carros", 5);
+        Veiculo corolla = new Veiculo("Corolla", 2019);
         Gson gson = new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
                 .setPrettyPrinting()
                 .create();
 
-        var tituloJson = gson.toJson(meuTitulo);
+        var corollaJson = gson.toJson(corolla);
 
-        System.out.println(tituloJson);
+        System.out.println(corollaJson);
 
     }
     }
