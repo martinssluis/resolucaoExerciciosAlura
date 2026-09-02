510e2f7f95f6ce97c41d3e7634fc42bab8ba70c7
Luis <martinsluisdev@gmail.com>
2026-09-02 15:21:35 -0300

    imprimindo resumo do livro

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,15 +1,15 @@
-import com.example.services.Produto;
+import com.example.entity.Livro;
 
 void main (){
 
     Scanner sc = new Scanner(System.in);
 
-    IO.print("Informe o nome do produto: ");
-    String nomeProduto = sc.nextLine();
-    IO.print("Informe o preço do produto: ");
-    double precoProduto = sc.nextDouble();
-    IO.print("Informe a quantidade do produto no estoque: ");
-    int quantidadeDeProdutos = sc.nextInt();
-    Produto mouse = new Produto(nomeProduto,precoProduto, quantidadeDeProdutos);
-    mouse.exibirRelatorio();
+    IO.print("Informe o nome do Livro: ");
+    String tituloLivro = sc.nextLine();
+    IO.print("Informe o autor do Livro: ");
+    String autorLivro = sc.nextLine();
+    IO.print("Informe a quantidade de produto páginas: ");
+    int quantidadeDePaginas = sc.nextInt();
+    Livro livro = new Livro(tituloLivro, autorLivro, quantidadeDePaginas);
+    livro.resumoDoLivro();
 }
