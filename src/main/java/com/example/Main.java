9f4dd86458f590ef8e876b072cb5f0fa22940730
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-12 16:40:07 -0300

    feat(Main): filtra produtos com preço menor que 1000 e ordena de forma crescente

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,26 @@
 package com.example;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        List<Produto> produtos = Arrays.asList(
+                new Produto("Smartphone", 800.0, "Eletrônicos"),
+                new Produto("Notebook", 1500.0, "Eletrônicos"),
+                new Produto("Teclado", 200.0, "Eletrônicos"),
+                new Produto("Cadeira", 300.0, "Móveis"),
+                new Produto("Monitor", 900.0, "Eletrônicos"),
+                new Produto("Mesa", 700.0, "Móveis")
+        );
 
+       List<Produto> menorMil = produtos.stream()
+               .filter(produto -> produto.getPreco() < 1000)
+               .sorted(Comparator.comparing(Produto::getPreco))
+               .collect(Collectors.toList());
+
+        System.out.println(menorMil);
     }
-    }
+}
